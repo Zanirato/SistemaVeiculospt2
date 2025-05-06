1d3fba9de61dc43b06d41c34955b8a64551e6149
Julia Zanirato <juliazaniratoo@gmail.com>
2025-05-06 09:02:17 -0300

    tela de login

--- views/template.php
+++ views/template.php
@@ -184,6 +184,8 @@ $usuario = Auth::getUsuario();
                 </div>
             </div>
         </div>
+
+
         <!-- Tabela de veículos cadastrados -->
         <div class="row mt-4">
             <div class="col-12">
@@ -250,5 +252,6 @@ $usuario = Auth::getUsuario();
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
